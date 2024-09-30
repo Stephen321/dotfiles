7f846618a665985b377433d429ca2891e5683e46
Stephen321 <Stephen321@users.noreply.github.com>
2024-09-30 14:33:24 +0100

    change pipx to uv

--- install_scripts/install.ps1
+++ install_scripts/install.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-scoop install zig fzf fd pipx
+scoop install zig fzf fd uv
 Install-Module -Name PowerShellGet -Force
 Install-Module -Name PSFzf 
 Install-Module lsd-aliases -Scope CurrentUser -AllowClobber
--- install_scripts/install.sh
+++ install_scripts/install.sh
@@ -1,4 +1,4 @@
 
-paru -S fzf fd pipx
+paru -S fzf fd uv
 cargo_dependencies=$(cat ./cargo_dependencies)
 carg install $cargo_dependencies
