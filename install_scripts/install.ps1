f77453351bef6d98658f6474d893a5b9b85486ba
Stephen321 <Stephen321@users.noreply.github.com>
2024-10-09 22:03:08 +0100

    more changes from the last week

--- install_scripts/install.ps1
+++ install_scripts/install.ps1
@@ -1,6 +1,7 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
-scoop install zig fzf fd uv
+scoop bucket add extras
+scoop install zig fzf fd uv lazygit
 Install-Module -Name PowerShellGet -Force
 Install-Module -Name PSFzf 
 Install-Module lsd-aliases -Scope CurrentUser -AllowClobber
